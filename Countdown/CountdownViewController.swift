9af7bd30ba5042bac44799b7ca92d2c5a6a55039
Hayden Hastings <haydenhastings32@gmail.com>
2019-05-08 11:37:48 -0600

    Timer now works
    Updated the start button to the countdown.
    Conformed to the delegate protocol

--- Countdown/CountdownViewController.swift
+++ Countdown/CountdownViewController.swift
@@ -12,18 +12,55 @@ class CountdownViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        countdown.delegate = self
+        
+        countdown.duration = 5 // seconds
+        timeLabel.font = UIFont.monospacedDigitSystemFont(ofSize: timeLabel.font.pointSize, weight: .regular)
+    }
+    
+    private func updateViews() {
+//        timeLabel.text = "\(countdown.timeRemaining)"
+        timeLabel.text = string(from: countdown.timeRemaining)
     }
 
     @IBAction func startButtonPressed(_ sender: Any) {
         print("start")
+        
+        countdown.start()
     }
     
     @IBAction func resetButtonPressed(_ sender: Any) {
         print("reset")
+        
+        
     }
+    
+    func string(from duration: TimeInterval) -> String {
+        let date = Date(timeIntervalSinceReferenceDate: duration)
+        return dateFormatter.string(from: date)
+    }
+    
+    var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm:ss.SS"
+        formatter.timeZone = TimeZone.init(secondsFromGMT: 0)
+        return formatter
+    }()
     
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var startButton: UIButton!
+    
+    let countdown = Countdown()
 }
 
+extension CountdownViewController: CountdownDelegate {
+    func countdownDidUpdate(timeRemaining: TimeInterval) {
+        updateViews()
+    }
+    
+    func countdownDidFinish() {
+        
+    }
+    
+    
+}
